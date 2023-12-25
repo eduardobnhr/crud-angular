98deb3577b9574708079812a86dd4ddee571df9f
eduardobnhr <eduardobnhr@gmail.com>
2023-12-25 12:39:57 -0300

    chamda http get nop angular e rxjs

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,20 +10,19 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses:  Observable<Course[]>;
+  //courses: Course[] = []
   displayedColumns = ['name', 'category'];
 
   //courseService: CoursesService;
 
   constructor(private courseService: CoursesService){
-
     //this.courseService = new CoursesService();
-    //this.courses = this.courseService.list();
-
+    this.courses = this.courseService.list();
+    //this.courseService.list().subscribe(courses => this.courses = courses);
    }
 
   ngOnInit(): void {
-    this.courses = this.courseService.list();
   }
 
 }
